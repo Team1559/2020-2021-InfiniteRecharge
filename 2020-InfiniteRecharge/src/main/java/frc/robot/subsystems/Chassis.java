f17007205254a548465dacee2b333fc876ada873
SonicDRJ <32721112+SonicDRJ@users.noreply.github.com>
2020-01-15 19:38:10 -0500

    Added REVRobotics Implementation (#7)
    * Phoenix Installed and built
    
    Installed Phoenix and built it!
    
    * Implemented Chassis into subsystem folder
    
    * Added REVRobotics implementation

--- 2020-InfiniteRecharge/src/main/java/frc/robot/subsystems/Chassis.java
+++ 2020-InfiniteRecharge/src/main/java/frc/robot/subsystems/Chassis.java
@@ -2,6 +2,12 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+
+import edu.wpi.first.wpilibj.Spark;
+
 public class Chassis {
-    private WPI_TalonSRX motor;
+    private WPI_TalonSRX motor1;
+    private WPI_TalonSRX motor2;
+    private Spark spark1;
+    private Spark spark2;
 }

8863e4f0760cfe68e46d2c2f1a1eef92e3bfe277
SonicDRJ <32721112+SonicDRJ@users.noreply.github.com>
2020-01-18 13:54:35 -0500

    Sonic drj branch (#23)
    * Phoenix Installed and built
    
    Installed Phoenix and built it!
    
    * Implemented Chassis into subsystem folder
    
    * Added REVRobotics implementation
    
    * Implemented Oblog, added potential logging of speed controllers
    
    Added the implementation necessary to use Oblog, and attempted to write code that may log data into Shuffleboard.
    
    * Editting Chassis code to make it work better
    
    Needs Joystick control.
    
    * Working on making working code for motors
    
    * changed op to oi
    
    Co-authored-by: Rylan Meilutis <44624900+Rylan-Meilutis@users.noreply.github.com>

--- 2020-InfiniteRecharge/src/main/java/frc/robot/Robot.java
+++ 2020-InfiniteRecharge/src/main/java/frc/robot/Robot.java
@@ -7,11 +7,20 @@
 
 package frc.robot;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.subsystems.Chassis;
+import io.github.oblarg.oblog.*;
+
 import frc.robot.subsystems.Shooter;
-import frc.robot.subsystems.Intake; 
+import frc.robot.subsystems.Intake;
+import frc.robot.OperatorInterface;
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the TimedRobot
@@ -24,6 +33,12 @@ public class Robot extends TimedRobot {
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
+  private CANSparkMax spark1;
+  private CANSparkMax spark2;
+  private Chassis driveTrain;
+  private OperatorInterface oi;
+
+
 
   /**
    * This function is run when the robot is first started up and should be
@@ -34,6 +49,12 @@ public class Robot extends TimedRobot {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
+    Logger.configureLoggingAndConfig(this, false);
+    spark1 = new CANSparkMax(11, MotorType.kBrushless);
+    spark2 = new CANSparkMax(12, MotorType.kBrushless);
+    driveTrain = new Chassis(spark1, spark2);
+    oi = new OperatorInterface();
+
   }
 
   /**
@@ -45,7 +66,9 @@ public class Robot extends TimedRobot {
    * LiveWindow and SmartDashboard integrated updating.
    */
   @Override
-  public void robotPeriodic() {
+  public void robotPeriodic()
+  {
+    Logger.updateEntries();
   }
 
   /**
@@ -93,8 +116,9 @@ public class Robot extends TimedRobot {
    * This function is called periodically during test mode.
    */
   @Override
-  public void testPeriodic() {
-  
+  public void testPeriodic() 
+  {
+    driveTrain.DriveSystem(oi.pilot);
   }
   @Override
   public void disabledInit(){

05dab93a7e0feeb157bab28992efb479d454c9d9
SonicDRJ <32721112+SonicDRJ@users.noreply.github.com>
2020-01-25 10:29:16 -0500

    Working Chassis Code (#36)
    * Phoenix Installed and built
    
    Installed Phoenix and built it!
    
    * Implemented Chassis into subsystem folder
    
    * Added REVRobotics implementation
    
    * Implemented Oblog, added potential logging of speed controllers
    
    Added the implementation necessary to use Oblog, and attempted to write code that may log data into Shuffleboard.
    
    * Editting Chassis code to make it work better
    
    Needs Joystick control.
    
    * Working on making working code for motors
    
    * Working motors!!!!
    
    * Consolidation
    
    Taking out the SparkMaxs from the robot class and shoving them into the chassis class.
    
    * added extra motors for chassis, started button tapped code
    
    Added two extra motors for the chassis motors. Started putting in code to make a toggle for the mode changer.
    
    * Added drive train chooser and PID
    
    Added the Drive Train selector into the robot.java allowing the drive train type to be chosen before the robot is enabled. Also, added PID thingies to the sparkMax motors, but I have no idea if they work or how they work.
    
    * Added stuff
    
    * Added Motor Widgets, removed @logs and @configs from code
    
    Added motor widgets the old school way and removed the Oblog logging thingies. I don't know how to use Oblog, and I understand this way at least a little bit, so plz don't bite me or kill me in my sleep
    
    * Did things, made changes
    
    Added a differential drive widget (maybe) and set the default value for the motor widgets to zero instead of one (to avoid the motors going full send on some poor unsuspecting person)
    
    * Working Drive Code
    
    This includes working drive code for Tank, arcade, and curvature drives! It also has Shuffleboard control of the motors individually and in their controller groups, allowing for better testing of the motors. All control done through Shuffleboard is done by old school widget control, so DO NOT DELETE THE WIDGET CLASSES unless you can successfully re-create the work done by the widgets with Oblog. Since I don't know how to use Oblog, I used the old fashioned way.
    
    * Working Chassis Code!!!!
    
    This is working chassis code!!!! Due to an unfamiliarity with Oblog, I am still NOT USING Oblog for my widgets... I DID ADD the neccessary code to use Oblog, but have decided to comment it out for now for my own sake... this is viable to change in the future, just be made aware that this is how it is for now.
    
    * Working Chassis Code
    
    This Chassis code works! Due to how the acceleration thingy works, that is NOT in the code. That is done through the REV Robotics Spark Max Client, so make sure that they are properly set up. (Mine was set to 0.4 seconds to go from 0 to 100%, which I believe was in Gianluca's safe motor speed range. Will Need a chassis to test that for sure)

--- 2020-InfiniteRecharge/src/main/java/frc/robot/Robot.java
+++ 2020-InfiniteRecharge/src/main/java/frc/robot/Robot.java
@@ -13,6 +13,8 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.subsystems.Shooter;
@@ -33,12 +35,28 @@ import frc.robot.components.Camera;
  * project.
  */
 public class Robot extends TimedRobot {
+  private static final String kDefaultAuto = "Default";
+  private static final String kCustomAuto = "My Auto";
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+  private Chassis driveTrain;
+  private OperatorInterface oi;
+
+  private static final String kTankDrive = "Tank Drive";
+  private static final String kArcadeDrive = "Arcade Drive";
+  private static final String kCurvatureDrive = "Curvature Drive";
+  private static final String kShuffleDrive = "Shuffle Drive Individual";
+  private static final String kShuffleDriveGroups = "Shuffle Drive Control Groups";
+  private String m_driveTrain;
+  private final SendableChooser<String> m_driveChooser = new SendableChooser<>();
+
+  private ShuffleboardTab driveTrainTab;
+
   private Camera camera1;
   private Camera camera2;
 
   private IMU imu;
-  public OperatorInterface oi;
-  private Chassis driveTrain;
+
   private boolean chassisEnable = false;
   private boolean ImuEnable = false;
   private boolean camera1Enable = false;
@@ -49,9 +67,11 @@ public class Robot extends TimedRobot {
   public void Enable_IMU(boolean enable){
     ImuEnable = enable;
   }
-  @Config
+  @Config.ToggleButton
   public void Enable_Chassis(boolean enable){
     chassisEnable = enable;
+    System.out.println("Chassis Enable: " + chassisEnable);
+    System.out.println("Enable: " + enable);
   }
   @Config
   public void Enable_Cameras(boolean enable, boolean enable2){
@@ -65,9 +85,27 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
-    imu = new IMU();
-    oi = new OperatorInterface();
+    driveTrainTab = Shuffleboard.getTab("Drive Train"); //The Shuffleboard Tab for all Drive Train related stuff
+
+    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    m_chooser.addOption("My Auto", kCustomAuto);
+    SmartDashboard.putData("Auto choices", m_chooser);
+
     Logger.configureLoggingAndConfig(this, false);
+
+    driveTrain = new Chassis();
+
+    oi = new OperatorInterface();
+
+    m_driveChooser.setDefaultOption("Tank Drive",kTankDrive); //A Drive Train option
+    m_driveChooser.addOption("Arcade Drive", kArcadeDrive); //A Drive Train option
+    m_driveChooser.addOption("Curvature Drive", kCurvatureDrive); //A Drive Train option
+    m_driveChooser.addOption("Shuffle Drive Individual", kShuffleDrive); //A Drive Train option
+    m_driveChooser.addOption("Shuffle Drive Control Groups", kShuffleDriveGroups); //A Drive Train option
+    driveTrainTab.add("Drive Train Choices", m_driveChooser); //Allows you to pick a Drive Train option through Shuffleboard
+
+    imu = new IMU();
+  }
     camera1 = new Camera(0);
     camera2 = new Camera(1);
 }
@@ -81,10 +119,11 @@ public class Robot extends TimedRobot {
    * LiveWindow and SmartDashboard integrated updating.
    */
   @Override
-  public void robotPeriodic() {
-    
+  public void robotPeriodic()
+  {
     Logger.updateEntries();
-    System.out.println(ImuEnable);
+
+    //System.out.println(ImuEnable);
   }
     
   /**
@@ -99,26 +138,31 @@ public class Robot extends TimedRobot {
    * SendableChooser make sure to add them to the chooser code above as well.
    */
   @Override
-  public void autonomousInit() {
+  public void autonomousInit()
+  {
+    m_autoSelected = m_chooser.getSelected();
+    m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
+    System.out.println("Auto selected: " + m_autoSelected);
+
     initialize();
     
     
     
-    if(ImuEnable){
+    if(ImuEnable)
+    {
     imu.zeroYaw();
-    
-  }
+    }
   }
 
   /**
    * This function is called periodically during autonomous.
    */
   @Override
-  public void autonomousPeriodic() {
+  public void autonomousPeriodic()
+  {
     if(ImuEnable){
       imu.getvalues();
     }
-    
   }
 
   /**
@@ -126,53 +170,70 @@ public class Robot extends TimedRobot {
    */
   
   @Override
-  public void teleopInit() {
+  public void teleopInit()
+  {
     if(robotInitialized == false){
       initialize();
     }
   }
   @Override
-  public void teleopPeriodic() {
+  public void teleopPeriodic()
+  {
+    driveTrain.DriveSystem(oi.pilot);
     if(ImuEnable){
       imu.getvalues();
     }
-    
   }
   
   /**
    * This function is called periodically during test mode.
    */
+
   @Override
-  public void testInit() {
+  public void testInit()
+  {
+    m_driveTrain = m_driveChooser.getSelected();
     initialize();
   }
+
   @Override
   public void testPeriodic() 
   {
     if(chassisEnable){
-      driveTrain.DriveSystem(oi.pilot);
+      driveTrain.DriveSystem(oi.pilot,m_driveTrain);
     }
   }
 
   @Override
-  public void disabledInit(){
+  public void disabledInit()
+  {
 
   }
   @Override
-  public void disabledPeriodic(){
+  public void disabledPeriodic()
+  {
 
   }
-  public void initialize(){
+  public void initialize()
+  {
     robotInitialized = true;
     if(ImuEnable){
       imu.init();
     }
+
+    System.out.println("Initilied");
+    if(chassisEnable)
+    {
+      driveTrain.Init();
+    }
+    System.out.println("ChassisEnable: " + chassisEnable);
+
     if(camera1Enable){
       camera1.init();
     }
     if(camera2Enable){
       camera2.init();
     }
-    System.out.println("Initilized");
+
   }
 }

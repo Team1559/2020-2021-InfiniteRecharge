57ae5046fa39d8b30dfd995e4431deb3a6a5b074
Eric-Marchetti <44625126+Eric-Marchetti@users.noreply.github.com>
2020-02-05 17:43:35 -0500

    Created AUto.java

---
+++ 2020-InfiniteRecharge/src/main/java/frc/robot/subsystems/Auto.java
@@ -0,0 +1,17 @@
+package frc.robot.subsystems;
+
+import frc.robot.subsystems.Chassis;
+import frc.robot.subsystems.PowerCell;
+import frc.robot.components.Camera;
+import frc.robot.components.IMU;
+import frc.robot.OperatorInterface;
+import frc.robot.Wiring;
+import io.github.oblarg.oblog.Loggable;
+import io.github.oblarg.oblog.Logger;
+import io.github.oblarg.oblog.annotations.Config;
+import io.github.oblarg.oblog.annotations.Log;
+import io.github.oblarg.oblog.annotations.Log.Logs;
+
+public class Auto implements Loggable{
+    
+}

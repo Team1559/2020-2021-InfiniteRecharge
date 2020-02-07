e46b046a435daaa3638bb8e4d82e062f62d224e8
Rylan Meilutis <meilutisrt@gmail.com>
2020-02-07 12:14:40 -0500

    removed unnessisary imports

--- 2020-InfiniteRecharge/src/main/java/frc/robot/subsystems/PowerCell.java
+++ 2020-InfiniteRecharge/src/main/java/frc/robot/subsystems/PowerCell.java
@@ -6,7 +6,6 @@ import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
 import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
-import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import frc.robot.OperatorInterface;
 import frc.robot.Wiring;
